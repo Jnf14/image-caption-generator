f03c9a7a4b600df47ac07bb2d9a7ea0e65d31074
Jnf14 <jeremie.frei@epfl.ch>
2024-11-03 19:23:04 +0100

    Remove unused testing

--- download-model.py
+++ download-model.py
@@ -16,37 +16,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 print("Model downloaded successfully!")
 
-# test the model
-print("Testing the model...")
-device = "cpu"
-model.to(device)
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-image = Image.open("images/cat.png")
-pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values
-pixel_values = pixel_values.to(device)
-
-output_ids = model.generate(pixel_values, **gen_kwargs)
-
-preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-captions = [pred.strip() for pred in preds]
-
-print("The given caption is: ", captions)
-print("Model tested successfully!")
-
 # save the model to local directory
 print("Saving the model to local directory...")
 classifier = pipeline('image-to-text', model=model, tokenizer=tokenizer, feature_extractor=feature_extractor)
 classifier.save_pretrained(model_path)
 print("Model saved successfully!")
-
-
-# # load model from local directory if it works
-# model = VisionEncoderDecoderModel.from_pretrained(model_path, local_files_only=True)
-# print("-----------  model loaded from local dir ------------")
-# tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-# print("-----------  tokenizer loaded from local dir ------------")
-# classifier = pipeline('image-to-text' ,model=model, tokenizer=tokenizer)
-
-# classifier(["good"])
